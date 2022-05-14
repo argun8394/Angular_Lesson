7238a5f8c10f95d5f0aba6e0ab39fdba1ecc98a7
argun8394 <argundoganali@gmail.com>
2022-05-14 23:44:25 +0300

    routing

--- routing/src/app/servers/servers.component.ts
+++ routing/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import {ActivatedRoute, Router} from '@angular/router'
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    //Using Relative Paths in Programmatic Navigation
+   // this.router.navigate(['servers'], {relativeTo:this.route})//Relative path i belirtitken 2. parmetre olarak javascript objesi olan {relativeTo:this.route} kullanılmalıdır
+
   }
 
 }
--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.user ={//******fetching route parameters ******
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    }
+
+
+    /*Paramss here is an observable
+
+observables are a feature added by some other third party package, not by Angular but heavily used by Angular which allow you to easily work with asynchronous tasks ,
+
+this therefore actually is the approach you should take to be really safe against changes not being, reflected in your template ,
+Paramss burada gözlemlenebilir
+
+gözlemlenebilirler, başka bir üçüncü taraf paketi tarafından eklenen, Angular tarafından değil, Angular tarafından yoğun olarak kullanılan ve asenkron görevlerle kolayca çalışmanıza izin veren bir özelliktir.
+
+bu nedenle, şablonunuza yansıtılmayan değişikliklere karşı gerçekten güvenli olmak için izlemeniz gereken yaklaşım budur******* aşağıdaki metod içindir.********/
+    this.paramsSubscription=this.route.params.subscribe((params: Params) => {//Fetching Route Parameters Reactively
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    })
+  }
+
+  ngOnDestroy(){
+this.paramsSubscription.unsubscribe();
   }
 
 }
