6919c5fb0983006f9b31d016d5d1fe655efe5c90
argun8394 <argundoganali@gmail.com>
2022-05-20 14:49:40 +0300

    Template Driven Validation

--- angular forms/src/app/app.component.ts
+++ angular forms/src/app/app.component.ts
@@ -7,7 +7,10 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signUpForm: NgForm;//@ViewChild('') metodu ile formu isimlendirerek onsubmit() metodunda aşağıda ki gibi kullanabiliriz kullanabiliriz --> [2]
+  @ViewChild('f') signUpForm: NgForm;//@ViewChild('') metodu ile formu isimlendirerek onsubmit() metodunda aşağıda ki gibi kullanabiliriz kullanabiliriz ayrıca bu metodta HTMl de  onSubmit() içine f(yani lokal ref yazılmaz)  --> [2]
+  defaultQuestion='pet';
+  answer='';
+  genders= ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
