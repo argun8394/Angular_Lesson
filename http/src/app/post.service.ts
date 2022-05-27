c2936ad88673adfc92e8bb72b54a9136864722cc
argun8394 <argundoganali@gmail.com>
2022-05-27 15:23:13 +0300

    different types of responses

--- http/src/app/post.service.ts
+++ http/src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core"
 import { Subject, throwError } from "rxjs";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 import {Post} from './post.model'
 
 @Injectable({providedIn: 'root'})
@@ -11,8 +11,13 @@ export class PostsService{
 
   createAndStorePost(title: string,content: string){
     const postData: Post= {title: title,content: content};
-    this.http.post<{name: string}>('https://ng-complete-guide-6c377-default-rtdb.firebaseio.com/posts.json',postData).subscribe((response)=>{
-      console.log(response)
+    this.http.post<{name: string}>('https://ng-complete-guide-6c377-default-rtdb.firebaseio.com/posts.json',postData,
+    {//[*1] aşağıdaki delete kullandığımıza benzer
+      //Observing Different Types of Responses
+      observe:'response'//observe:'body' bu hali defaulttur response ta destekler bu value lar doğru yazılmazsa hata oluşur
+    }
+    ).subscribe((responseData)=>{
+      console.log(responseData);
     }, error => {
       this.error.next(error.message)
     })
@@ -46,7 +51,20 @@ export class PostsService{
   }
 
   deletePosts(){
-    return this.http.delete('https://ng-complete-guide-6c377-default-rtdb.firebaseio.com/posts.json');
+    return this.http.delete('https://ng-complete-guide-6c377-default-rtdb.firebaseio.com/posts.json',
+    //[*1] her zaman kullanılan bir yapı değil ama yardımcı olan bir yapı aşağıda tap ile kullandık
+    //Observing Different Types of Responses
+    {
+      observe:'events'//events dışında response ta alır //[*1]
+    } ).pipe(tap(event => {
+      console.log(event);
+      if(event.type === HttpEventType.Sent){//[*1]
+        //...
+      }
+      if(event.type === HttpEventType.Response){//[*1]
+        console.log(event.body)
+      }
+    }));
   }
 
   }
