78c648dfe8e35ca58dc3afa410c7c674d560f2c4
argun8394 <argundoganali@gmail.com>
2022-05-28 18:06:49 +0300

    Response Interceptors

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler){//intercept() metodu 2 parametre alır ve kullanımı şekilde olduğu gibidir
@@ -8,7 +9,13 @@ export class AuthInterceptorService implements HttpInterceptor{
       headers: req.headers.append('Auth', 'xyz') //(interceptor)önleyicimiz, tüm giden istekler için (Auth:xyz)bunu ekliyor ve bu nedenle, önleyiciler çok önemli ve kullanışlı bir özellik.
       //Browser da Network->Request Hedars ta incelediğimizde eklendiğini görürüz
     })
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap(event=>{
+      console.log(event);
+      if(event.type === HttpEventType.Response){// Sadece interceptor'ınızın içindeki istekle etkileşime geçemezsiniz, isterseniz yanıtla da etkileşime girebilirsiniz. bunu kaydedebilir, gereksinimleriniz ne olursa olsun manipüle edebilirsiniz.
+        console.log('Response arrived, body data');
+        console.log(event.body)
+      }
+    }));
   }
 
 }
