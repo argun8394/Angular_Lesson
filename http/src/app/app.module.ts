15ec0d194f090024b73209672dda15a28a73bf57
argun8394 <argundoganali@gmail.com>
2022-05-28 21:07:00 +0300

    multiple interceptors

--- http/src/app/app.module.ts
+++ http/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [
+    //Bu şekilde interceptor lerin provide sırasını  yaparsak -> burada sadece bir önleyicide ayarlamadığımız özel başlığı aldık, yine de auth başlığını kaçırıyoruz. Bu yüzden interceptorlerin privede sıra önemlidir, ancak bunun dışında, birden fazla önleyici sağlamak, anlayabileceğiniz gibi son derece basittir.
+    {provide:HTTP_INTERCEPTORS,
+      useClass:LoggingInterceptorService,
+      multi:true
+    },
     {provide:HTTP_INTERCEPTORS,
       useClass:AuthInterceptorService,
       multi:true
--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -3,19 +3,20 @@ import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler){//intercept() metodu 2 parametre alır ve kullanımı şekilde olduğu gibidir
-    console.log('Request is on its way');
-    console.log(req.url);
+    // console.log('Request is on its way');
+    // console.log(req.url);
     const modifiedRequest = req.clone({//clone() JS objesi alır
       headers: req.headers.append('Auth', 'xyz') //(interceptor)önleyicimiz, tüm giden istekler için (Auth:xyz)bunu ekliyor ve bu nedenle, önleyiciler çok önemli ve kullanışlı bir özellik.
       //Browser da Network->Request Hedars ta incelediğimizde eklendiğini görürüz
     })
-    return next.handle(modifiedRequest).pipe(tap(event=>{
-      console.log(event);
-      if(event.type === HttpEventType.Response){// Sadece interceptor'ınızın içindeki istekle etkileşime geçemezsiniz, isterseniz yanıtla da etkileşime girebilirsiniz. bunu kaydedebilir, gereksinimleriniz ne olursa olsun manipüle edebilirsiniz.
-        console.log('Response arrived, body data');
-        console.log(event.body)
-      }
-    }));
+    return next.handle(modifiedRequest)
+    // .pipe(tap(event=>{
+    //   console.log(event);
+    //   if(event.type === HttpEventType.Response){// Sadece interceptor'ınızın içindeki istekle etkileşime geçemezsiniz, isterseniz yanıtla da etkileşime girebilirsiniz. bunu kaydedebilir, gereksinimleriniz ne olursa olsun manipüle edebilirsiniz.
+    //     console.log('Response arrived, body data');
+    //     console.log(event.body)
+    //   }
+    // }));
   }
 
 }
